a90d783630cd17dececfdcdf544fc06115120f37
jsalgg <mzootfb@Lukes-MBP.attlocal.net>
2021-03-23 16:56:03 -0400

    proj complkete

--- app.js
+++ app.js
@@ -14,12 +14,29 @@ app.get("/", (req, res) => {
 
 app.get("/*xyz", (req, res) => {
   res.send(`That's all I wrote.`);
-})
+});
 
 app.get("/capital-letters/:str", (req, res) => {
   // let upperCased = `${req.params.str}`.toUpperCase();
   res.send(`${req.params.str}`.toUpperCase());
-})
+});
+
+app.get(["/margot/:str", "/margeaux/:str"], (req, res) => {
+  //   if (!req.path.toLowerCase().startsWith("/margot")) {
+  //     res.send(
+  //       `${req.params.str}`.charAt(0).toUpperCase() +
+  //         `${req.params.str}`.substr(1).toLowerCase()
+  //     );
+  //   }
+  res.send(
+    `${req.params.str}`.charAt(0).toUpperCase() +
+      `${req.params.str}`.substr(1).toLowerCase()
+  );
+});
+
+app.get("/about/foo", (req, res) => {
+  res.send(404);
+});
 
 app.all("*", (req, res) => {
   //   console.log(req.method);
